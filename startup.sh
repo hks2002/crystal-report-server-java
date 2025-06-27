68adca89efa42f70af752290a6efc3bfc77a246e
Robert Huang <56649783@qq.com>
2025-06-27 14:39:05 +0800

    fix: 🐛update the shell script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 ./shutdown.sh
-java -jar crystal-report-server-java.jar --conf=config-prod.json --options=vertx-options.json &
+java -jar crystal-report-server-java.jar --conf=config-prod.json --options=vertx-options.json
